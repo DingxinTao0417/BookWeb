a6a10f8713c475ba5a7bdf7048477100cd8f3b18
Dingxin Tao <taodingxin0417@gmail.com>
2025-05-26 00:14:28 -0700

    添加bookId，error变量 使controller能面对不同情况处理异常

--- Book/console/src/main/java/com/example/book/console/domain/ConsoleStatusVo.java
+++ Book/console/src/main/java/com/example/book/console/domain/ConsoleStatusVo.java
@@ -3,8 +3,12 @@ package com.example.book.console.domain;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.math.BigInteger;
+
 @Data
 @Accessors(chain = true)
 public class ConsoleStatusVo {
+    private BigInteger bookId;
+    private String error;
     private String status;
 }
