3ad7442232d35f5f825ef353ccff151b74611140
Dingxin Tao <taodingxin0417@gmail.com>
2025-06-27 22:01:13 -0700

    添加category controller

---
+++ Book/app/src/main/java/com/example/book/app/controller/CategoryController.java
@@ -0,0 +1,54 @@
+package com.example.book.app.controller;
+
+import java.math.BigInteger;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import com.example.book.app.domain.*;
+import com.example.book.module.entity.Book;
+import com.example.book.module.entity.Category;
+import com.example.book.module.service.BookService;
+import com.example.book.module.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    public CategoryController() {
+    }
+
+    @RequestMapping({"/category/list"})
+    public BookCategoryListVo categoryAll() {
+        List<Category> categoryList= this.categoryService.getAllCategory();
+
+        List<BookCategoryDetailsVo> bookCategoryVoList = new ArrayList();
+        Iterator var3 = categoryList.iterator();
+
+        while(var3.hasNext()) {
+            Category category = (Category)var3.next();
+            BookCategoryDetailsVo categoryDetailsVo = new BookCategoryDetailsVo();
+            categoryDetailsVo.setCategoryId(category.getId());
+            categoryDetailsVo.setCategoryName(category.getCategoryName());
+            categoryDetailsVo.setCategoryImages(category.getCategoryImages().split("\\$")[0]);
+
+            bookCategoryVoList.add(categoryDetailsVo);
+        }
+
+        BookCategoryListVo bookCategoryListVo = new BookCategoryListVo();
+        bookCategoryListVo.setList(bookCategoryVoList);
+        return bookCategoryListVo;
+    }
+}
---
+++ Book/console/src/main/java/com/example/book/console/controller/CategoryController.java
@@ -0,0 +1,90 @@
+package com.example.book.console.controller;
+
+import com.example.book.console.domain.*;
+import com.example.book.module.entity.Book;
+import com.example.book.module.entity.Category;
+import com.example.book.module.service.BookService;
+import com.example.book.module.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import com.example.book.module.service.Result;
+
+@Slf4j
+@RestController
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @RequestMapping("/category/add")
+    public BigInteger categoryCreate(@RequestParam(name = "images") String images,
+                                     @RequestParam(name = "categoryName") String categoryName) {
+        ConsoleStatusVo consoleStatusVo = new ConsoleStatusVo();
+        try {
+            BigInteger returnedCategoryId = categoryService.createCategory(images, categoryName);
+            return returnedCategoryId;
+        } catch (RuntimeException e){
+            return BigInteger.ZERO;
+        }
+    }
+
+    @RequestMapping("/category/update")
+    public ConsoleStatusVo categoryUpdate(@RequestParam(name = "categoryId") BigInteger categoryId,
+                                          @RequestParam(name = "images") String images,
+                                          @RequestParam(name = "categoryName") String categoryName) {
+        ConsoleStatusVo consoleStatusVo = new ConsoleStatusVo();
+        try {
+            BigInteger returnedCategoryId = categoryService.updateCategory(categoryId, images, categoryName);
+            consoleStatusVo.setCategoryId(returnedCategoryId);
+            return consoleStatusVo;
+        } catch (RuntimeException e){
+            consoleStatusVo.setError(e.getMessage());
+            return consoleStatusVo;
+        }
+    }
+
+    @RequestMapping("/category/delete")
+    public ConsoleStatusVo categoryDelete(@RequestParam(name = "categoryId") BigInteger categoryId) {
+        int status = categoryService.delete(categoryId);
+        ConsoleStatusVo consoleInfoVo = new ConsoleStatusVo();
+        consoleInfoVo.setStatus(1 == status ? "成功" : "失败");
+        return consoleInfoVo;
+    }
+
+    @RequestMapping({"/category/list"})
+    public ConsoleCategoryListVo categoryAll(@RequestParam(name = "page", defaultValue = "1") int page,
+                                             @RequestParam(name = "pageSize", defaultValue = "10") int pageSize) {
+        int offset = (page - 1) * pageSize;
+        List<Category> categoryList = this.categoryService.getByOffset(offset, pageSize);
+
+        List<ConsoleCategoryDetailsVo> bookCategoryVoList = new ArrayList();
+        Iterator var3 = categoryList.iterator();
+
+        while(var3.hasNext()) {
+            Category category = (Category)var3.next();
+            ConsoleCategoryDetailsVo categoryDetailsVo = new ConsoleCategoryDetailsVo();
+            categoryDetailsVo.setCategoryId(category.getId());
+            categoryDetailsVo.setCategoryName(category.getCategoryName());
+            categoryDetailsVo.setCategoryImages(category.getCategoryImages().split("\\$")[0]);
+
+            bookCategoryVoList.add(categoryDetailsVo);
+        }
+
+        ConsoleCategoryListVo bookCategoryListVo = new ConsoleCategoryListVo();
+        bookCategoryListVo.setList(bookCategoryVoList);
+        return bookCategoryListVo;
+    }
+
+}
