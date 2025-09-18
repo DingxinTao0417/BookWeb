ee7ad58f944513e63d3ae890989b1b548f2e90f5
Dingxin Tao <taodingxin0417@gmail.com>
2025-09-17 23:32:11 -0700

    新建BookDto储存基本信息及分类名称

---
+++ Book/module/src/main/java/com/example/book/module/DTO/BookDto.java
@@ -0,0 +1,18 @@
+package com.example.book.module.DTO;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.math.BigInteger;
+
+@Data
+@Accessors(chain = true)
+public class BookDto {
+    private BigInteger bookId;
+    private String images;
+    private String bookTitle;
+    private Integer bookRating;
+    private String bookCategory;
+    private Integer createTime;
+    private Integer updateTime;
+}
